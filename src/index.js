5644b113a334694c8f921f34e61abfa2b2a1b673
NikitaHordiienko <nikita.gordienko34@gmail.com>
2022-09-14 21:38:45 +0300

    Готовое 10 дз

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries.js'
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import createdCountryList from './templates/countryList.hbs';
+import createdCountryInfo from './templates/countryInfo.hbs';
 
+const inputEl = document.querySelector('#search-box');
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+
+function clearInput() {
+  countryListEl.innerHTML = '';
+  countryInfoEl.innerHTML = '';
+}
+
+inputEl.addEventListener('input', debounce(event => {
+    const valueWithTrim = inputEl.value.trim();
+
+    clearInput()
+    if (valueWithTrim !== '') {
+        fetchCountries(valueWithTrim).then(data => {
+            if (data.length > 10) {
+                Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+            } else if (data.length === 0) {
+                Notiflix.Notify.failure('Oops, there is no country with that name')
+            } else if (data.length >= 2 && data.length <= 10) {
+                console.log(data)
+                countryListEl.innerHTML = createdCountryList(data)
+            } else if (data.length === 1) {                
+                countryInfoEl.innerHTML = createdCountryInfo(data)
+            }
+        })
+    }
+}, DEBOUNCE_DELAY))
+
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,20 @@
+'use strict';
+
+export const fetchCountries = countryName => {
+    return fetch(
+        `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`
+    )
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 404) {
+                    return []
+                }
+                throw new Error(response.status)
+            }
+
+            return response.json();
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
